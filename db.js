38b6e546f3a1f86cf10332f72894622d0cef6f5e
pushkar <tiwaripushker@gmail.com>
2018-05-03 21:33:08 +0530

    worked on the mongodb constant file

--- db.js
+++ db.js
@@ -1,22 +1,10 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-// Connection URL
-const url = 'mongodb://localhost:27017';
-// Database Name
-const dbName = 'node';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function (err, client) {
-  assert.equal(null, err);
-  console.log("connected sucessfully to server");
-
-  const db = client.db(dbName);
-  client.close();
-});
-module.exports = function (callback) {
-    if (db || error) {
-        callback(error, db);
-    } else {
-        waiting.push(callback);
-    }
-}
+module.exports = {
+    "host": "localhost",
+    "port": 27017,
+    "url" : "mongodb://localhost:27017/node",
+    "database": "node",
+    "password": "",
+    "name": "nodeMongoDB",
+    "user": "",
+    "connector": "mongodb"
+  };
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var CircularJSON = require('circular-json');
-// Use connect method to connect to the server
-// MongoClient.connect(url, function (err, client) {
-//   assert.equal(null, err);
-//   console.log("connected sucessfully to server");
-
-//   var db = client.db(dbName);
-//  // client.close();
-// });
-
+var moment = require('moment');
+var dbConnect = require('../db');
+var db;
+MongoClient.connect(dbConnect.url, (err, database) => {
+    if (err)
+        return console.log(err)
+    db = database
+});
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  let db = await MongoClient.connect("mongodb://localhost:27017/node");
-
-  let obj = await db.collection('users').find({}).toArray();
+  let obj = await db.collection('privateLabel').find({}).toArray();
   let response = JSON.stringify(obj);
   return res.send(response);
 });
 
-router.post('/get', async function (req, res) {
-  let db = await MongoClient.connect("mongodb://localhost:27017/node");
-   let response = await db.collection('users').insertOne({
-    name: req.body.name
+router.post('/addLabel', async function (req, res) {
+   let response = await db.collection('privateLabel').insertOne({
+    seller: req.body.seller,
+    product: req.body.product,
+    referral:req.body.referral,
+    referral_url:req.body.referral_url,
+    date_added:  moment(Date.now()).format('YYYY-MM-DD')
   });
   res.send('true');
 });
